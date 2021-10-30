422a7426897d55350c27ef60a68d0aa2eee24200
Shkredov Dmitriy <shkredovdmitriy@gmail.com>
2021-10-30 14:18:58 +0300

    add loupe

--- dev/app/animation.js
+++ dev/app/animation.js
@@ -12,7 +12,7 @@ if (document.querySelector(".container3")) {
       ease: "none",
       pin: true, // pin the trigger element while active
       start: "top 95%", // when the top of the trigger hits the top of the viewport
-      end: "top -150%", // end after scrolling 500px beyond the start
+      end: "top -130%", // end after scrolling 500px beyond the start
       scrub: 0 // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     }
   });
@@ -25,9 +25,19 @@ if (document.querySelector(".container3")) {
     .to(container2, 1, { autoAlpha: 0, ease: "none" })
     .to(container2, 0.25, { autoAlpha: 0, ease: "none" })
     .to(container3, 1, {
-      maskSize: "auto +=200%",
+      maskSize: "auto +=160%",
       maskPosition: "52% 50%",
       ease: "none"
     })
     .to(container3, 1, { autoAlpha: 0, ease: "none" }, "-=0.7");
 }
+
+$("body").mousemove(function(event) {
+  x = event.offsetX;
+  y = event.offsetY;
+  console.log(x);
+  $(".loupe-container-2").css("mask-position", `${x}px ${y}px`);
+  $("img.loupe")
+    .css("top", `${y}px`)
+    .css("left", `${x}px`);
+});
--- dev/pages/index/index.ts
+++
@@ -1,7 +0,0 @@
-import "~/common/scss/main.scss";
-//@ts-ignore
-require("~/app/_helpers/svg-sprite");
-
-import log from "~/app/_helpers/log";
-
-log("TEST TITLE", "Test body");
